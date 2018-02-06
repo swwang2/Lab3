c1cca551ee596498aa554015ec560a8d0e434525
Eddie Wang <swwang2@illinois.edu>
2018-02-06 14:31:24 -0600

    incomplete

--- src/WebScrapper.java
+++ src/WebScrapper.java
@@ -1,5 +1,46 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScrapper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static int wordCount(final String url) {
+        String contents = urlToString(url);
+        String[] words = contents.split(" ");
+        int count = 0;
+        boolean letters;
+        for (int i = 0; i < words.length; i++) {
+            letters = true;
+            for (int j = 0; j < words[i].length(); j++) {
+                if (!Character.isDefined(words[i].charAt(j))) {
+                    letters = false;
+                }
+            }
+            if (letters) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] unused) {
-        System.out.println("WebScrapper ");
+//        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        System.out.println(wordCount("https://www.bls.gov/tus/charts/chart9.txt"));
     }
 }
